e552ff5bdbe34fe22e5d598f83c203ee1e162a04
Rowen Bakkeren <9018934@student.zadkine.nl>
2025-12-09 15:38:57 +0100

    route change

--- easyeats/src/App.jsx
+++ easyeats/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+import Profile from "./pages/Profile";
 import RecipeDetail from "./pages/DetailRecipe";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
             <Route path='/recipe/:id' element={<RecipeDetail />} />
         </Routes>
       </BrowserRouter>
